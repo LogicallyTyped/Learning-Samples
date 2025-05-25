302e408f965382db77a9b138a2b4987ad17ba11b
Christian Chavero Perez <Christian.ChaveroPerez@libertyenergy.com>
2025-05-25 19:12:27 -0400

    Add ExceptionsDemo project with exception handling
    This commit introduces a new project called "ExceptionsDemo" that demonstrates exception handling using the `DemoCode` class from the "ExceptionsLibrary". The `Program.cs` file includes a `Main` method that calls a method which may throw an exception, handling it by printing the error message and stack trace. The "ExceptionsLibrary" project contains methods that simulate potential exceptions by accessing an array with an out-of-bounds index. Additionally, several configuration files, including `App.config`, were added or modified to support the new projects and their settings.

---
+++ ExceptionsDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using ExceptionsLibrary;
+using System;
+
+namespace ExceptionsDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            DemoCode demo = new DemoCode();
+
+            try
+            {
+                int result = demo.GrandparentMethod(4); // This will throw an exception if the index is out of bounds with a 4
+                Console.WriteLine($"The Value at the given position is: {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExceptionsLibrary/DemoCode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExceptionsLibrary
+{
+    public class DemoCode
+    {
+        public int GrandparentMethod(int position)
+        {
+            return ParentMethod(position);
+        }
+
+        public int ParentMethod(int position)
+        {
+            return GetNumber(position);
+        }
+
+        public int GetNumber(int position)
+        {
+            int output = 0;  // Default value in case of an exception
+
+            // We don't to try-catch at this level, the information about the exception should be handled by the caller.
+            //try
+            //{
+            int[] numbers = new int[] { 1, 4, 7, 2 };
+                output = numbers[position];
+            //}
+            //catch (System.Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Console.WriteLine(ex.StackTrace);  // where the exception happened.
+
+            //}
+
+            return output; // Return the output, which will be 0 if an exception occurred This makes the program worst.
+        }
+    }
+}
