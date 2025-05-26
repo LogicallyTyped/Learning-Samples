2f89530c002155676660da5ebd34920f25a3329b
Christian Chavero Perez <Christian.ChaveroPerez@libertyenergy.com>
2025-05-26 12:18:33 -0400

    tre

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿using ExceptionsLibrary;
+
+class Program
+{
+     static void Main(string[] args)
+    {
+        DemoCode demoCode = new DemoCode();
+
+        int result = demoCode.GrandparentMethod(3);
+
+        Console.WriteLine($"The value at given position is {result}");
+
+        Console.ReadLine();
+    }
+}
